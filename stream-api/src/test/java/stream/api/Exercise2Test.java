4723c2dcd827b118cbecf70799d6cc53524dacd2
Adam Rojewski <adam.a.rojewski@gmail.com>
2019-04-04 18:35:27 +0200

    third commit

--- stream-api/src/test/java/stream/api/Exercise2Test.java
+++ stream-api/src/test/java/stream/api/Exercise2Test.java
@@ -5,7 +5,6 @@ import common.test.tool.dataset.ClassicOnlineStore;
 import common.test.tool.entity.Customer;
 import common.test.tool.entity.Item;
 import common.test.tool.util.AssertUtil;
-
 import org.junit.Test;
 
 import java.util.Comparator;
@@ -15,66 +14,78 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class Exercise2Test extends ClassicOnlineStore {
 
-    @Easy @Test
+    @Easy
+    @Test
     public void sortByAge() {
         List<Customer> customerList = this.mall.getCustomerList();
+
 
         /**
          * Create a stream with ascending ordered age values.
          * Use {@link Stream#sorted} to sort them.
          */
-        Stream<Integer> sortedAgeStream = null;
+        Stream<Integer> sortedAgeStream = customerList.stream().map(Customer::getAge).sorted();
 
         List<Integer> sortedAgeList = sortedAgeStream.collect(Collectors.toList());
         assertThat(sortedAgeList, contains(21, 22, 22, 26, 27, 28, 32, 35, 36, 38));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void descSortByAge() {
         List<Customer> customerList = this.mall.getCustomerList();
 
         /**
          * Create a stream with descending ordered age values.
          */
-        Comparator<Integer> descOrder = null;
-        Stream<Integer> sortedAgeStream = null;
+        //Comparator<Integer> descOrder = null; chyba zbedne????
+        Stream<Integer> sortedAgeStream = customerList.stream().map(Customer::getAge).sorted(Comparator.reverseOrder());
 
-        assertTrue(AssertUtil.isLambda(descOrder));
+        //assertTrue(AssertUtil.isLambda(descOrder)); - chyba zbedne???
         List<Integer> sortedAgeList = sortedAgeStream.collect(Collectors.toList());
         assertThat(sortedAgeList, contains(38, 36, 35, 32, 28, 27, 26, 22, 22, 21));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void top3RichCustomer() {
         List<Customer> customerList = this.mall.getCustomerList();
+
 
         /**
          * Create a stream with top 3 rich customers using {@link Stream#limit} to limit the size of the stream
          */
-        Stream<String> top3RichCustomerStream = null;
+        Stream<String> top3RichCustomerStream = customerList.stream()
+                .sorted(Comparator.comparingInt(Customer::getBudget).reversed())
+                .map(Customer::getName).limit(3);
 
         List<String> top3RichCustomerList = top3RichCustomerStream.collect(Collectors.toList());
         assertThat(top3RichCustomerList, contains("Diana", "Andrew", "Chris"));
     }
 
-    @Easy @Test
+    @Easy
+    @Test
     public void distinctAge() {
         List<Customer> customerList = this.mall.getCustomerList();
 
         /**
          * Create a stream with distinct age values using {@link Stream#distinct}
          */
-        Stream<Integer> distinctAgeStream = null;
+        Stream<Integer> distinctAgeStream = customerList.stream()
+                .map(Customer::getAge).distinct();
 
         List<Integer> distinctAgeList = distinctAgeStream.collect(Collectors.toList());
         assertThat(distinctAgeList, contains(22, 27, 28, 38, 26, 32, 35, 21, 36));
     }
+//TODO
 
-    @Easy @Test
+    @Easy
+    @Test
     public void itemsCustomersWantToBuy() {
         List<Customer> customerList = this.mall.getCustomerList();
 
@@ -88,9 +99,9 @@ public class Exercise2Test extends ClassicOnlineStore {
         assertTrue(AssertUtil.isLambda(getItemStream));
         List<String> itemList = itemStream.collect(Collectors.toList());
         assertThat(itemList,
-                   contains("small table", "plate", "fork", "ice cream", "screwdriver", "cable", "earphone", "onion",
-                            "ice cream", "crisps", "chopsticks", "cable", "speaker", "headphone", "saw", "bond",
-                            "plane", "bag", "cold medicine", "chair", "desk", "pants", "coat", "cup", "plate", "fork",
-                            "spoon", "ointment", "poultice", "spinach", "ginseng", "onion"));
+                contains("small table", "plate", "fork", "ice cream", "screwdriver", "cable", "earphone", "onion",
+                        "ice cream", "crisps", "chopsticks", "cable", "speaker", "headphone", "saw", "bond",
+                        "plane", "bag", "cold medicine", "chair", "desk", "pants", "coat", "cup", "plate", "fork",
+                        "spoon", "ointment", "poultice", "spinach", "ginseng", "onion"));
     }
 }
